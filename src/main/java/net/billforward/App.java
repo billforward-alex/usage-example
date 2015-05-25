fb412d983ae74f2427d2140ead6b2585f4eefc6c
Alex Birch <alexander.birch@billforward.net>
2015-05-25 13:05:06 +0100

    Make pageable entity iterator

--- src/main/java/net/billforward/App.java
+++ src/main/java/net/billforward/App.java
@@ -1,12 +1,11 @@
 package net.billforward;
 
+import com.zoominfo.util.yieldreturn.Generator;
 import net.billforward.exception.*;
+import net.billforward.model.BillingEntity;
 import net.billforward.model.Invoice;
 
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -35,16 +34,61 @@ public class App
 
 
     public static void getPreviousInvoice(Invoice currentInvoice) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
-        String queryString = String.format("?%s", Stream.of(
-                new AbstractMap.SimpleEntry<>("records", 1),
-                new AbstractMap.SimpleEntry<>("offset", 0),
-                new AbstractMap.SimpleEntry<>("order_by", "created"),
-                new AbstractMap.SimpleEntry<>("order", "DESC"),
-                new AbstractMap.SimpleEntry<>("include_retired", false)
-        ).map(x -> x.toString()).reduce((x, y) -> {
-            return String.format("%s&%s", x, y);
-        }).get());
-        Invoice[] i = Invoice.getBySubscriptionID(currentInvoice.getSubscriptionID());
-        System.out.println(Arrays.toString(i));
+        //fetchEntitiesUntilConditionFulfilled<Invoice>(currentInvoice.getSubscriptionID());
+        //System.out.println(Arrays.toString(i));
+    }
+
+    public static class EntityPagingHelper<T extends BillingEntity> {
+        public Iterable<T> fetchEntitiesUntilConditionFulfilled(EntityFetcher<T> entityFetcher, String nominalInput, EntitySuitableLambda<Invoice> condition) {
+            return new Generator<T>() {
+                @Override protected void run() {
+                    Iterator<T[]> iterator = fetchPagesUntilConditionFulfilled(entityFetcher, nominalInput).iterator();
+                    while(iterator.hasNext()) {
+                        T[] page = iterator.next();
+                        Arrays.stream(page)
+                                .forEach(thisPage -> yield(thisPage));
+                    }
+                }
+            };
+        }
+
+        public Iterable<T[]> fetchPagesUntilConditionFulfilled(EntityFetcher<T> entityFetcher, String nominalInput) {
+            int pageLimit = 1;
+            int pageSize = 1;
+            return new Generator<T[]>() {
+                @Override protected void run() {
+                    for (int i = 0; i < pageLimit; i++) {
+                        try {
+                            yield(getPage(entityFetcher, nominalInput, i, pageSize));
+                        } catch (Exception e) {
+                            return;
+                        }
+                    }
+                }
+            };
+        }
+
+        interface EntitySuitableLambda<T> {
+            boolean run(T input);
+        }
+
+        interface EntityFetcher<T> {
+            T[] run(String nominalInput);
+        }
+
+        public T[] getPage(EntityFetcher<T> entityFetcher, String nominalInput, int page, int pageSize) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
+            String queryString = String.format("%s?%s", nominalInput,
+                    Stream.of(
+                            new AbstractMap.SimpleEntry<>("records", pageSize),
+                            new AbstractMap.SimpleEntry<>("offset", page*pageSize),
+                            new AbstractMap.SimpleEntry<>("order_by", "created"),
+                            new AbstractMap.SimpleEntry<>("order", "DESC"),
+                            new AbstractMap.SimpleEntry<>("include_retired", false)
+                    ).map(x -> x.toString()).reduce((x, y) -> {
+                        return String.format("%s&%s", x, y);
+                    }).get()
+            );
+            return entityFetcher.run(queryString);
+        };
     }
 }
