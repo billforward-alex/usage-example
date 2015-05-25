6eaea80cc322437aedd697124efb4d34f3cc5355
Alex Birch <alexander.birch@billforward.net>
2015-05-25 21:00:11 +0100

    Reduce dependencies

--- src/main/java/net/billforward/App.java
+++ src/main/java/net/billforward/App.java
@@ -1,9 +1,7 @@
 package net.billforward;
 
 import com.sun.deploy.util.StringUtils;
-import com.zoominfo.util.yieldreturn.Generator;
 import net.billforward.exception.*;
-import net.billforward.model.BillingEntity;
 import net.billforward.model.Invoice;
 
 import java.util.*;
@@ -19,11 +17,9 @@ public class App
     {
         BillForwardClient.makeDefaultClient("48ccf496-26b9-42f5-b11e-56a6a2171db8", "http://local.billforward.net:8089/RestAPI");
 
-        App myApp = new App();
-
         try {
             Invoice currentInvoice = getCurrentInvoice();
-            Invoice previousInvoice = myApp.getPreviousInvoice(currentInvoice);
+            Invoice previousInvoice = getPreviousInvoice(currentInvoice);
             System.out.println(StringUtils.join(Arrays.asList(
                 "Departed period:",
                 String.format("\tStart:\t%s", previousInvoice.getPeriodStart()),
@@ -43,15 +39,42 @@ public class App
         return Invoice.getByID("INV-0A46471A-5702-49E2-AED4-3DBE57E7");
     }
 
-    public Invoice getPreviousInvoice(Invoice currentInvoice) {
-        EntityPagingHelper<Invoice> pagingHelper = new EntityPagingHelper<>(e -> {
-            System.err.println("An error occurred");
-            System.err.println(e);
-        }, 10, 200);
-        return pagingHelper.fetchFirstEntityMeetingCondition(invoice -> !invoice.getID().equalsIgnoreCase(currentInvoice.getID())
-                && invoice.getPeriodStart().before(currentInvoice.getPeriodStart())
-                && invoice.getPeriodEnd().equals(currentInvoice.getPeriodStart())
-                && invoice.getType().equals(Invoice.InvoiceType.Subscription), Invoice::getBySubscriptionID, currentInvoice.getSubscriptionID());
-
+    public static Invoice getPreviousInvoice(Invoice currentInvoice) throws Exception {
+        if (currentInvoice.getInitialInvoice()) {
+            throw new Exception("This is the first invoice; no previous invoice will exist.");
+        }
+        int pageSize = 1;
+        int pageLimit = 100;
+        for (int i=0; i<pageLimit; i++) {
+            Invoice[] page = getPage(currentInvoice.getSubscriptionID(), i, pageSize);
+            if (page == null || page.length < 1) {
+                throw new Exception("Reached an empty page, and no matching entity exists");
+            }
+            for(Invoice invoice : page) {
+                if (!invoice.getID().equalsIgnoreCase(currentInvoice.getID())
+                        && invoice.getPeriodStart().before(currentInvoice.getPeriodStart())
+                        && invoice.getPeriodEnd().equals(currentInvoice.getPeriodStart())
+                        && invoice.getType().equals(Invoice.InvoiceType.Subscription)) {
+                    return invoice;
+                }
+            }
+            if (page.length < pageSize) {
+                throw new Exception("Reached end of final page, and no matching entity exists");
+            }
+        }
+        throw new Exception("Page limit reached without every encountering a matching entity");
     }
+
+    public static Invoice[] getPage(String nominalInput, int page, int pageSize) throws BillforwardException {
+        return Invoice.getBySubscriptionID(StringUtils.join(Arrays.asList(nominalInput,
+                Stream.of(
+                        new AbstractMap.SimpleEntry<>("records", pageSize),
+                        new AbstractMap.SimpleEntry<>("offset", page * pageSize),
+                        new AbstractMap.SimpleEntry<>("order_by", "created"),
+                        new AbstractMap.SimpleEntry<>("order", "DESC"),
+                        new AbstractMap.SimpleEntry<>("include_retired", false)
+                ).map(x -> x.toString()).reduce((x, y) -> StringUtils.join(Arrays.asList(x, y), "&")).get()), "?"));
+    };
+
+
 }
